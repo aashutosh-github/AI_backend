5704ab41138b163a8be1744a4f71a8fd105cb5c6
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2026-07-30 23:48:00 +0530

    create auth middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import User from "../model/userSchema.js";
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+
+    if (!token) {
+      return res.status(401).json({ message: "User not logged in" });
+    }
+
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    const existingUser = await User.findById(payload.id);
+
+    if (!existingUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    req.user = existingUser;
+
+    next();
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export default authMiddleware;
