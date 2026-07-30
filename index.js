f3a7dd40027e3d93dec2b17eb9889efe6f193951
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2026-07-30 23:47:45 +0530

    configure user router

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./config/mongoose.js";
 import userRouter from "./routes/userRouter.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/user", userRouter);
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
+import authMiddleware from "../middlewares/authUserMiddleware.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/signup", signUp);
 userRouter.post("/login", logIn);
-userRouter.post("/profile", profile);
+userRouter.post("/profile", authMiddleware, profile);
 userRouter.post("/logout", logOut);
 
 export default userRouter;
