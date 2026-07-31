a1f5ddff12e99f9cb1f79d394196b6bc30967d09
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2026-07-31 22:45:53 +0530

    setting up validators

---
+++ validators/userValidators.js
@@ -0,0 +1,33 @@
+import z from "zod";
+
+export const signupSchema = z.object({
+  name: z.string().trim().min(3).max(30).toLowerCase(),
+  age: z.number().min(10).max(100).optional(),
+  email: z.email().trim().toLowerCase(),
+  password: z
+    .string()
+    .regex(/[A-Z]/, "There should be at least one capital letter in password")
+    .regex(/[a-z]/, "There should be at least one small letter in password")
+    .regex(/[0-9]/, "There should be at least one number in password")
+    .regex(
+      /[!@#$%^&*=?""]/,
+      "There should be at least one special letter in password",
+    )
+    .min(8)
+    .max(30),
+});
+
+export const loginSchema = z.object({
+  email: z.email().trim().toLowerCase(),
+  password: z
+    .string()
+    .regex(/[A-Z]/, "There should be at least one capital letter in password")
+    .regex(/[a-z]/, "There should be at least one small letter in password")
+    .regex(/[0-9]/, "There should be at least one number in password")
+    .regex(
+      /[!@#$%^&*=?""]/,
+      "There should be at least one special letter in password",
+    )
+    .min(8)
+    .max(30),
+});
