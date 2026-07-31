09ee32d84a8b749bca5a8c7dbd02547eecf023d0
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2026-08-01 00:02:56 +0530

    create chat controllers
    create chat, get most recent 20 chats, delete chat, get one chat are the
    functions that have been defined here

---
+++ controllers/chatControllers.js
@@ -0,0 +1,82 @@
+import Chat from "../model/chatSchema.js";
+import Message from "../model/messageSchema.js";
+
+export const getRecentChats = async (req, res) => {
+  try {
+    const chats = await Chat.find({ userId: req.user.id })
+      .select("topic updatedAt")
+      .sort({ updatedAt: -1 })
+      .limit(20);
+
+    return res.status(200).json({
+      message: "Your 20 recent chats",
+      chats,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const createChat = async (req, res) => {
+  try {
+    const createdChat = await Chat.create({ userId: req.user.id });
+    res.status(201).json({
+      message: "Chat created successfully",
+      chat: {
+        userId: createdChat.userId,
+        chatId: createdChat._id,
+        createdAt: createdChat.createdAt,
+        topic: createdChat.topic,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const deleteChat = async (req, res) => {
+  try {
+    const { chatId } = req.params;
+
+    const chat = await Chat.findOne({ _id: chatId, userId: req.user.id });
+    if (!chat) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to perform this action" });
+    }
+
+    await Message.deleteMany({ chatId: chat._id });
+    await Chat.deleteOne({ _id: chat._id });
+
+    res.status(200).json({ message: "Chat deleted successfully" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getSingleChat = async (req, res) => {
+  try {
+    const { chatId } = req.params;
+
+    const chat = await Chat.findOne({
+      _id: chatId,
+      userId: req.user.id,
+    });
+    if (!chat) {
+      return res.status(404).json({ message: "No such chat found" });
+    }
+
+    return res.status(200).json({
+      userId: chat.userId,
+      chatId: chat._id,
+      topic: chat.topic,
+      usage: chat.usage,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
