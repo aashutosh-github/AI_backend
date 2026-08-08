2563effbe9d89c35a72ae30c990b05e3748dbf67
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2026-08-08 13:44:25 +0530

    correct method for fetching profile details and auth before logout

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,13 +1,13 @@
 import express from "express";
-import authMiddleware from "../middlewares/authUserMiddleware.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
 import * as userController from "../controllers/userControllers.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/signup", userController.signUp);
 userRouter.post("/login", userController.logIn);
-userRouter.post("/profile", authMiddleware, userController.profile);
-userRouter.post("/logout", userController.logOut);
+userRouter.get("/profile", authMiddleware, userController.profile);
+userRouter.post("/logout", authMiddleware, userController.logOut);
 userRouter.delete("/delete", authMiddleware, userController.deleteProfile);
 
 export default userRouter;
