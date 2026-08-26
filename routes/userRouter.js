b559da6b7ac79a0408755232c1604a0e3ac8af2d
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2026-08-26 20:53:31 +0530

    add unauthenticated user rate limiter in user routes

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,11 +1,16 @@
 import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
 import * as userController from "../controllers/userControllers.js";
+import unauthenticatedUserRateLimiter from "../middlewares/unauthenticatedUserRateLimiter.js";
 
 const userRouter = express.Router();
 
-userRouter.post("/signup", userController.signUp);
-userRouter.post("/login", userController.logIn);
+userRouter.post(
+  "/signup",
+  unauthenticatedUserRateLimiter,
+  userController.signUp,
+);
+userRouter.post("/login", unauthenticatedUserRateLimiter, userController.logIn);
 userRouter.get("/profile", authMiddleware, userController.profile);
 userRouter.post("/logout", authMiddleware, userController.logOut);
 userRouter.delete("/delete", authMiddleware, userController.deleteProfile);
