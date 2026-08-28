b005873321acba0e39e7a403817dc19e021a09da
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2026-08-28 19:03:32 +0530

    Use rate limiter and load user data middlewares in user routes

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,18 +1,34 @@
 import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
 import * as userController from "../controllers/userControllers.js";
-import unauthenticatedUserRateLimiter from "../middlewares/unauthenticatedUserRateLimiter.js";
+import unauthenticatedRateLimiter from "../middlewares/unauthenticatedUserRateLimiter.js";
+import loadUserDataIntoReq from "../middlewares/loadUserDataIntoReq.js";
+import authenticatedRateLimiter from "../middlewares/authenticatedUserRateLimiter.js";
 
 const userRouter = express.Router();
 
-userRouter.post(
-  "/signup",
-  unauthenticatedUserRateLimiter,
-  userController.signUp,
+userRouter.post("/signup", unauthenticatedRateLimiter, userController.signUp);
+userRouter.post("/login", unauthenticatedRateLimiter, userController.logIn);
+userRouter.get(
+  "/profile",
+  authMiddleware,
+  authenticatedRateLimiter,
+  loadUserDataIntoReq,
+  userController.profile,
 );
-userRouter.post("/login", unauthenticatedUserRateLimiter, userController.logIn);
-userRouter.get("/profile", authMiddleware, userController.profile);
-userRouter.post("/logout", authMiddleware, userController.logOut);
-userRouter.delete("/delete", authMiddleware, userController.deleteProfile);
+userRouter.post(
+  "/logout",
+  authMiddleware,
+  authenticatedRateLimiter,
+  loadUserDataIntoReq,
+  userController.logOut,
+);
+userRouter.delete(
+  "/delete",
+  authMiddleware,
+  authenticatedRateLimiter,
+  loadUserDataIntoReq,
+  userController.deleteProfile,
+);
 
 export default userRouter;
