86537225bb023f41a3f7b4d4ca514625063a9bef
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2026-09-01 15:18:08 +0530

    apply rate limiting while sending messages

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,13 +1,31 @@
 import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
 import * as messageController from "../controllers/messageControllers.js";
+import authenticatedUserRateLimiter from "../middlewares/authenticatedUserRateLimiter.js";
+import loadUserDataIntoReq from "../middlewares/loadUserDataIntoReq.js";
+import tokenUsageRateLimiter from "../middlewares/tokenUsageRateLimiter.js";
 
 const messageRouter = express.Router();
 
 messageRouter.use(authMiddleware);
+messageRouter.use(authenticatedUserRateLimiter);
 
-messageRouter.post("/", messageController.sendMessage);
-messageRouter.get("/:chatId", messageController.getAllMessages);
-messageRouter.post("/:chatId", messageController.sendMessage);
+messageRouter.post(
+  "/",
+  tokenUsageRateLimiter,
+  loadUserDataIntoReq,
+  messageController.sendMessage,
+);
+messageRouter.get(
+  "/:chatId",
+  loadUserDataIntoReq,
+  messageController.getAllMessages,
+);
+messageRouter.post(
+  "/:chatId",
+  tokenUsageRateLimiter,
+  loadUserDataIntoReq,
+  messageController.sendMessage,
+);
 
 export default messageRouter;
