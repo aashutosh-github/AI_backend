78a11f9b39a8829ba615cf9bed911e8db7246120
Aashutosh <175815744+aashutosh-github@users.noreply.github.com>
2026-09-01 13:51:08 +0530

    add rate limiter to chat router

--- routes/chatRouter.js
+++ routes/chatRouter.js
@@ -1,10 +1,14 @@
 import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
 import * as chatController from "../controllers/chatControllers.js";
+import authenticatedUserRateLimiter from "../middlewares/authenticatedUserRateLimiter.js";
+import loadUserDataIntoReq from "../middlewares/loadUserDataIntoReq.js";
 
 const chatRouter = express.Router();
 
 chatRouter.use(authMiddleware);
+chatRouter.use(authenticatedUserRateLimiter);
+chatRouter.use(loadUserDataIntoReq);
 
 chatRouter.post("/create", chatController.createChat);
 chatRouter.get("/recents", chatController.getRecentChats);
